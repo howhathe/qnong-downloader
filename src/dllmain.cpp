89a14b08fde51ca3c933d96f56101c0e141dc157
Dulfy <68484326+leginsoffi@users.noreply.github.com>
2022-12-01 22:43:42 +0300

    1.3 Update
    Full links support & Fix bug with auto-id (conflict)

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,15 +1,15 @@
 
 #include "nong.h"
 
-int songId = 0, autoId = 0;
+int idSong = 0, idAuto = 0, previousSong = 0;
 std::string youtubeLink = "";
 std::string audioQuality = "128k";
-const char *qualityList[] = { "128k\0", "192k\0", "256k\0", "320k\0", nullptr };
+const char *qualityList[] = { "128k\0", "192k\0", "256k\0", "320k\0" };
 
 bool __fastcall PlayLayer::initHook(gd::PlayLayer* self, int edx, gd::GJGameLevel* level) {
-	bool out = PlayLayer::init(self, level); if (!out) { return out; }
+	bool out = PlayLayer::init(self, level);
 
-    autoId = gd::GameManager::sharedState()->getPlayLayer()->m_level->songID;
+    idAuto = self->m_level->songID;
 
     return out;
 }
@@ -17,69 +17,77 @@ bool __fastcall PlayLayer::initHook(gd::PlayLayer* self, int edx, gd::GJGameLeve
 DWORD WINAPI mainThread(void* hModule) {
     using namespace MegaHackExt;
 
-    Window* window = Window::Create("qNoNG | 1.2");
+    Window* window = Window::Create("qNoNG | 1.3");
 
-    TextBox* inputSongId = TextBox::Create("Song ID");
-    inputSongId->setCallback([](TextBox* a, const char* Slider) {
-        songId = atoi(Slider);
+    TextBox* inputIdSong = TextBox::Create("Song ID"); auto idSongstr = std::to_string(idSong); inputIdSong->set(idSongstr.c_str());
+    inputIdSong->setCallback([](TextBox* a, const char* Slider) {
+        idSong = atoi(Slider);
     });
 
-    CheckBox* boxAutoSongID = CheckBox::Create("Auto ID");
-    boxAutoSongID->setCallback([](CheckBox* a, bool status) {
-        if (status)
-            songId = autoId;
+    CheckBox* boxAutoidSong = CheckBox::Create("Auto ID");
+    boxAutoidSong->setCallback([](CheckBox* a, bool status) {
+        if (status) {
+            previousSong = idSong;
+            idSong = idAuto;
+        } else {
+            idSong = previousSong;
+        } 
     });
 
-    TextBox* inputLink = TextBox::Create("YouTube Link");
+    TextBox* inputLink = TextBox::Create("YouTube Link"); inputLink->set(youtubeLink.c_str());
     inputLink->setCallback([](TextBox* a, const char* Slider) {
         youtubeLink = Slider;
     });
 
     ComboBox *qualityCombo = ComboBox::Create("Quality: ", nullptr);
 	qualityCombo->setCallback([](ComboBox *obj, int index, const char *str) {
-        if (index == 0)
-            audioQuality = "128k";
-        else if (index == 1)
-            audioQuality = "192k";
-        else if (index == 2)
-            audioQuality = "256k";
-        else if (index == 3)
-            audioQuality = "320k";
+        if (index == 0) audioQuality = "128k";
+        else if (index == 1) audioQuality = "192k";
+        else if (index == 2) audioQuality = "256k";
+        else if (index == 3) audioQuality = "320k";
     });
 	qualityCombo->setValues(qualityList);
 
     Button* downloadSong  = Button::Create("Download");
 	downloadSong ->setCallback([](Button* a) {
-        std::string outputLine = "start qNoNG\\start.bat " + std::to_string(songId) + " " + audioQuality + " " + youtubeLink;
+        if (youtubeLink.find("https://www.youtube.com/watch?v=") != std::string::npos)
+            youtubeLink = youtubeLink.substr(32, 11);
+        else if (youtubeLink.find("https://youtu.be/") != std::string::npos)
+            youtubeLink = youtubeLink.substr(17, 11);
+        else if (youtubeLink.find("https://www.youtube.com/watch?v=") != std::string::npos)
+            youtubeLink = youtubeLink.substr(0, 11);
+        else if (youtubeLink.find("https://www.youtube.com/watch?v=") != std::string::npos)
+            youtubeLink = youtubeLink.substr(0, 11);
+
+        std::string outputLine = "start qNoNG\\start.bat " + std::to_string(idSong) + " " + audioQuality + " " + youtubeLink;
         system(outputLine.c_str());
     });
 
     Button* deleteSong = Button::Create("Delete");
 	deleteSong->setCallback([](Button* a) {
-        std::string delCommand = "Resources\\" + std::to_string(songId) + ".mp3";
+        std::string delCommand = "Resources\\" + std::to_string(idSong) + ".mp3";
         int result = std::filesystem::remove(delCommand.c_str());
 
         if (result == 0) {
-            std::string messageText = "Song: \"" + std::to_string(songId) + "\" not found.";
-            MessageBoxA(NULL, messageText.c_str(), "qNoNG 1.2", MB_OK | MB_ICONERROR);
+            std::string messageText = "Song: \"" + std::to_string(idSong) + "\" failed to delete.";
+            MessageBoxA(NULL, messageText.c_str(), "qNoNG 1.3", MB_OK | MB_ICONERROR);
         } else {
-            std::filesystem::remove(delCommand.c_str());
-            std::string messageText = "Song: \" " + std::to_string(songId) + " \" deleted!";
-            MessageBoxA(NULL, messageText.c_str(), "qNoNG 1.2", MB_OK | MB_ICONINFORMATION);
+            std::string messageText = "Song: \" " + std::to_string(idSong) + " \" deleted!";
+            MessageBoxA(NULL, messageText.c_str(), "qNoNG 1.3", MB_OK | MB_ICONINFORMATION);
         }
     });
     
 	window->add(HorizontalLayout::Create(downloadSong, deleteSong));
     window->add(qualityCombo);
     window->add(inputLink);
-    window->add(HorizontalLayout::Create(inputSongId, boxAutoSongID));
+    window->add(HorizontalLayout::Create(inputIdSong, boxAutoidSong));
 
     Client::commit(window);
 
     return TRUE;
 }
 
-void mh_init() {
+void MinHook_Setup() {
     MH_CreateHook(
         (PVOID)(gd::base + 0x01FB780),
         PlayLayer::initHook,
@@ -96,7 +104,7 @@ BOOL APIENTRY DllMain(HMODULE hModule,
     {
     case DLL_PROCESS_ATTACH:
         if (MH_Initialize() == MH_OK) {
-            mh_init();
+            MinHook_Setup();
             MH_EnableHook(MH_ALL_HOOKS);
         }
 
